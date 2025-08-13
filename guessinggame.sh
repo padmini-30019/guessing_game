77e6f2fb716676eca5dafdad0aec0cfe3cad2a21
padmini-30019 <2420030019@klh.edu.in>
2025-08-13 22:32:03 +0530

    fixed code

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
-count_files(){
-echo $(ls -1 | wc -1)
+
+# Function to count only regular, non-hidden files in current directory
+count_files() {
+    find . -maxdepth 1 -type f ! -name ".*" | wc -l
 }
+
+# Store the actual number
 actual=$(count_files)
+
 echo "Welcome to the Guessing Game!"
 echo "Guess how many files are in the current directory."
-while true; do
-read -p "Enter your guess:" guess
-if ![["$guess"=~^[0-9]+$]]; then
-echo "Invalid input. Please enter a number."
-continue
-fi
-if[[$guess -lt $actual ]];then
-echo "Too low!"
-elif[[$guess -ggt $actual ]];then
-echo "Too high!"
-else
-echo "Congratulations! You guessed correctly."
-break
-fi
-done
 
+while true; do
+    read -p "Enter your guess: " guess
+
+    # Ensure input is a number
+    if ! [[ "$guess" =~ ^[0-9]+$ ]]; then
+        echo "Please enter a valid number."
+        continue
+    fi
+
+    if [[ $guess -lt $actual ]]; then
+        echo "Too low!"
+    elif [[ $guess -gt $actual ]]; then
+        echo "Too high!"
+    else
+        echo "Congratulations! You guessed correctly."
+        break
+    fi
+done
